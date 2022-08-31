0e8bc98c2eb3646c368b894d021a8b49f93069b5
maxgmr <maxgmr@students.cs.ubc.ca>
2022-08-31 11:58:06 -0600

    Displays basic roster info

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
+use minidom::Element;
+use roster::load_ros::Roster;
 use std::env;
 use std::process;
 
 mod roster;
+
+const LINE: &'static str = "=======";
 
 fn main() -> std::io::Result<()> {
     let args: Vec<String> = env::args().collect();
@@ -11,12 +15,16 @@ fn main() -> std::io::Result<()> {
     });
     file_name.push_str(".ros");
 
-    let roster = roster::load_ros::read_ros(&file_name).unwrap_or_else(|err| {
+    let ros_str = roster::load_ros::read_ros(&file_name).unwrap_or_else(|err| {
         eprintln!("Problem reading .ros file: {err}");
         process::exit(1);
     });
 
-    println!("{roster}");
+    let root: Element = roster::load_ros::get_xml(&ros_str);
+
+    let roster: Roster = roster::load_ros::create_roster(&root);
+
+    display_roster(&roster);
 
     Ok(())
 }
@@ -30,4 +38,14 @@ fn parse_args(args: &[String]) -> Result<String, std::io::Error> {
     }
     let file_name: String = args[1].clone();
     Ok(file_name)
+}
+
+fn display_roster(roster: &Roster) {
+    println!("{}{}{}", LINE, roster.get_name(), LINE);
+    println!("Power Level: {:.1}", roster.get_power_level());
+    println!("Points: {:.1}", roster.get_points());
+    for force in roster.get_forces() {
+        println!("{}{}{}", LINE, force.get_name(), LINE);
+        println!("Faction: {}", force.get_faction());
+    }
 }
--- src/roster/load_ros.rs
+++ src/roster/load_ros.rs
@@ -1,6 +1,123 @@
+use minidom::Element;
 use std::fs;
+
+const NS: &'static str = "http://www.battlescribe.net/schema/rosterSchema";
+
+pub struct Roster {
+    name: String,
+    power_level: f64,
+    points: f64,
+    forces: Vec<Force>,
+}
+
+impl Roster {
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+    pub fn get_power_level(&self) -> &f64 {
+        &self.power_level
+    }
+    pub fn get_points(&self) -> &f64 {
+        &self.points
+    }
+    pub fn get_forces(&self) -> &Vec<Force> {
+        &self.forces
+    }
+}
+
+pub struct Force {
+    faction: String,
+    name: String,
+}
+
+impl Force {
+    pub fn get_faction(&self) -> &String {
+        &self.faction
+    }
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+}
 
 pub fn read_ros(file_name: &String) -> std::io::Result<String> {
     let ros_string = fs::read_to_string(file_name)?;
     return Ok(ros_string);
+}
+
+pub fn get_xml(s: &String) -> Element {
+    s.parse().unwrap()
+}
+
+pub fn create_roster(root: &Element) -> Roster {
+    let name: String = get_name(&root);
+    let (power_level, points) = get_costs(&root);
+    let forces: Vec<Force> = get_forces(&root);
+    Roster {
+        name,
+        power_level,
+        points,
+        forces,
+    }
+}
+
+fn get_name(r: &Element) -> String {
+    match r.attr("name") {
+        Some(n) => String::from(n),
+        None => String::from(""),
+    }
+}
+
+fn get_costs(r: &Element) -> (f64, f64) {
+    let mut pl: f64 = 0.0;
+    let mut pts: f64 = 0.0;
+    for c0 in r.children() {
+        if c0.is("costs", NS) {
+            for c1 in c0.children() {
+                if c1.is("cost", NS) {
+                    match c1.attr("value") {
+                        Some(v) => match c1.attr("name") {
+                            Some(" PL") => {
+                                pl = v.parse::<f64>().unwrap_or_else(|err| {
+                                    eprintln!("Problem parsing Power Level from .ros file: {err}");
+                                    0.0
+                                })
+                            }
+                            Some("pts") => {
+                                pts = v.parse::<f64>().unwrap_or_else(|err| {
+                                    eprintln!("Problem parsing Points from .ros file: {err}");
+                                    0.0
+                                })
+                            }
+                            Some(_) => (),
+                            None => (),
+                        },
+                        None => println!("No cost data found on roster"),
+                    }
+                }
+            }
+        }
+    }
+    (pl, pts)
+}
+
+fn get_forces(r: &Element) -> Vec<Force> {
+    let mut forces: Vec<Force> = vec![];
+    for c0 in r.children() {
+        if c0.is("forces", NS) {
+            for c1 in c0.children() {
+                if c1.is("force", NS) {
+                    let faction = match c1.attr("catalogueName") {
+                        Some(x) => String::from(x),
+                        None => String::from(""),
+                    };
+                    let name = match c1.attr("name") {
+                        Some(x) => String::from(x),
+                        None => String::from(""),
+                    };
+                    forces.push(Force { faction, name });
+                }
+            }
+        }
+    }
+    forces
 }
